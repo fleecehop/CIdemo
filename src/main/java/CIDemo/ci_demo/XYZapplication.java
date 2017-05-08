3a876bfff3e32b2a76b128d17a5a0a875e9f963f
fleecehop <fleecehop@DESKTOP-MN2VJ46>
2017-05-07 23:29:04 -0400

    initial commit

---
+++ src/main/java/CIDemo/ci_demo/XYZapplication.java
@@ -0,0 +1,13 @@
+package CIDemo.ci_demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class XYZapplication 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "XYZApplication ran!" );
+    }
+}
